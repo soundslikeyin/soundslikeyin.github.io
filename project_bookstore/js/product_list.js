2bff0097e87737864849443cfe83cc1f448bb22a
Yin.He <heyin.sg@gmail.com>
2021-11-20 11:41:06 +0800

    updated search to trigger on keyup

--- project_bookstore/js/product_list.js
+++ project_bookstore/js/product_list.js
@@ -14,3 +14,145 @@ items.forEach((el) => {
         next = next.nextElementSibling
     }
 })
+
+
+//For book search
+const row = document.getElementById('book-search')
+const key = "AIzaSyDgRDYqwNBJPN87HDc87Hkcs5gb00fe0uw"
+const resultsNum = document.getElementById('result-num')
+const searchForm = document.querySelector('#search-bar-input')
+const searchText = document.querySelector('#search-bar-input > input')
+
+// searchText.addEventListener("keydown", function (event) {
+//     if (event.key === "Enter") {
+//         event.preventDefault();
+//         // Do more work
+//         processSearch();
+//     }
+// });
+
+searchText.addEventListener("keyup", function (event) {
+    // if (event.key === "Enter") {
+        event.preventDefault();
+        // Do more work
+        processSearch();
+    // }
+});
+
+
+function processSearch() {
+    // create a function to remove all child nodes
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    // call function to remove child nodes from previous search
+    removeAllChildNodes(row);
+
+    // construct query text using form data from search bar
+    let queryText = ""
+    queryText = searchText.value.replace(/\s/g, '+')
+
+    getData(queryText);
+}
+
+
+async function getData(queryText) {
+    // const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=' + key)
+    // queryText = searchText.value.replace(/\s/g, '+')
+    console.log('queryText =' + queryText)
+
+    const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=' + queryText + '&key=' + key)
+    const data = await response.json();
+    const books = data.items;
+    let resultsCount = books.length
+    resultsNum.innerText = resultsCount
+
+    // console.log(data)
+    console.log(books);
+
+    books.forEach(book => {
+        console.log(resultsNum)
+
+        const title = book.volumeInfo.title
+        console.log(title)
+
+        const authors = book.volumeInfo.authors
+        console.log(authors.join(", "))
+
+        // console.log(book.contributor[1])
+
+        // const isbn = book.volumeInfo.industryIdentifiers.find(identifier => identifier.type == "ISBN_10").identifier;
+        // console.log(isbn)
+
+        // const bookCoverUrl = "https://covers.openlibrary.org/b/isbn/" + isbn + "-S.jpg"
+    });
+
+    // if fetch is successful
+    if (response.status >= 200 && response.status < 400) {
+
+        books.forEach(book => {
+            // Create a div for each column
+            const column = document.createElement('div')
+            column.setAttribute('class', 'col-md-4 col-lg-3')
+
+            // Create a div for each card
+            const card = document.createElement('div')
+            card.setAttribute('class', 'card')
+
+            // Create a div for each card-img
+            const cardImg = document.createElement('div')
+            cardImg.setAttribute('class', 'card-img')
+
+            // Create a div for each card-body
+            const cardBody = document.createElement('div')
+            cardBody.setAttribute('class', 'card-body')
+
+            const authors = book.volumeInfo.authors
+            const authorName = authors.join(", ")
+            // const isbn = book.volumeInfo.industryIdentifiers.find(identifier => identifier.type == "ISBN_10").identifier;
+
+            // create bookCover and set cover imageurl
+            const bookCover = document.createElement('img')
+            bookCover.setAttribute('class', 'img-fluid')
+
+            // const bookCoverUrl = "https://covers.openlibrary.org/b/isbn/" + isbn + "-M.jpg"
+            const bookCoverUrl = book.volumeInfo.imageLinks.thumbnail
+            bookCover.src = bookCoverUrl
+
+            // Create an h4 and set the text content to the title
+            const title = book.volumeInfo.title
+            const bookTitle = document.createElement('h5')
+            bookTitle.setAttribute('class', 'card-title')
+            bookTitle.textContent = title
+
+            // Create a p and set the text content to the authors name
+            const bookAuthors = document.createElement('p')
+            bookAuthors.setAttribute('class', 'card-text')
+            bookAuthors.innerText = `Author: ${authorName}`
+
+            // Append the cards to the container element
+            row.appendChild(column)
+            column.appendChild(card)
+            card.appendChild(cardImg)
+            card.appendChild(cardBody)
+
+            // Each card will contain an h1 and a p
+            cardBody.appendChild(bookTitle)
+            cardImg.appendChild(bookCover)
+            cardBody.appendChild(bookAuthors)
+
+        })
+    } else {
+        console.log('error')
+    }
+
+}
+// getData();
+
+
+
+
+
